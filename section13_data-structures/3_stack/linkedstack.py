4cf9196c14dee2a164454054b4a87a2290ed8779
Aquaerius SSL <aquaerius@acrypto.life>
2019-06-22 15:14:46 +0200

    Pass the tests

--- section13_data-structures/3_stack/linkedstack.py
+++ section13_data-structures/3_stack/linkedstack.py
@@ -18,7 +18,8 @@ class LinkedStack:
         self.__linked_list.add_start_to_list(node)
 
     def pop(self):
-        self.__linked_list.remove_start_from_list()
+
+        return self.__linked_list.remove_start_from_list()
 
     def print_details(self):
         self.__linked_list.print_list()
